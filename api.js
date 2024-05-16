bf5aee88928d9638c082e9c06aa1ecc1680cfe9d
cvSquiggles <jones.steven.alexander@gmail.com>
2024-05-16 01:15:46 -0400

    Pushing completed node express app to github for deployment on Herkou

--- api.js
+++ api.js
@@ -12,6 +12,7 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var cors = require('cors');
 var app = express();
+const port = process.env.PORT || 3000;
 var router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -31,7 +32,7 @@ router.use((request, response, next) => {
 //Pull all players from database
 router.route('/players').get((request, response)=>{
     Db.getPlayers().then(result => {
-        //console.log(result);
+        console.log(result);
         response.json(result[0]);
     })
 })
@@ -74,12 +75,56 @@ router.route('/addMatch/:p1Minutes/:p1Seconds/:p1Milli/:p1MatchScore/:p2Minutes/
 })
 
 //Update an existing match in the database
+//router.route('/addMatch/:p1Minutes/:p1Seconds/:p1Milli/:p1MatchScore/:p2Minutes/:p2Seconds/:p2Milli/:p2MatchScore/:roundCount/:p1/:p2').post((request, response)=>{
+//    Db.addMatch(request.params.p1Minutes, request.params.p1Seconds, request.params.p1Milli,
+//        request.params.p1MatchScore, request.params.p2Minutes, request.params.p2Seconds,
+//        request.params.p2Milli, request.params.p2MatchScore, request.params.roundCount, request.params.p1, request.params.p2
+//    ).then(result => {
+//        //console.log(result);
+//        response.json(result).status(201);
+//    })
+//})
 
+//Update match times on play or pause button hit
+router.route('/updateMatch_time/:p1Minutes/:p1Seconds/:p1Milli/:p2Minutes/:p2Seconds/:p2Milli/:gameCode').put((request, response)=>{
+    Db.updateMatch_time(request.params.p1Minutes, request.params.p1Seconds, request.params.p1Milli,
+        request.params.p2Minutes, request.params.p2Seconds, request.params.p2Milli, request.params.gameCode
+    ).then(result => {
+        //console.log(result);
+        response.json(result).status(201);
+    })
+})
+
+//Update match scores on submit round scores hit
+router.route('/updateMatch_score/:p1MatchScore/:p2MatchScore/:roundCount/:gameCode').put((request, response)=>{
+    Db.updateMatch_score(request.params.p1MatchScore, request.params.p2MatchScore, request.params.roundCount, request.params.gameCode
+    ).then(result => {
+        //console.log(result);
+        response.json(result).status(201);
+    })
+})
+
+//Update match winner on Report match hit
+router.route('/updateMatch_winner/:winner/:gameCode').put((request, response)=>{
+    Db.updateMatch_winner(request.params.winner, request.params.gameCode
+    ).then(result => {
+        //console.log(result);
+        response.json(result).status(201);
+    })
+})
+
+
+//localhost:8090/api/updateMatch_time/:14/:10/:9/:22/:21/:20/:1005
 
 // Declare port to listen on for local testing
-var port = process.env.PORT || 8090;
-app.listen(port);
-console.log('Order API is running at ' + port);
+//var port = process.env.PORT || 8090;
+//app.listen(port);
+//console.log('Order API is running at ' + port);
+
+// Declare port to listen on for Azure
+app.listen(port, () => {
+    console.log('Order API is running at ' + port);
+});
 
 //Db.getPlayers().then(result => {
 //    console.log(result);
--- dboperations.js
+++ dboperations.js
@@ -60,7 +60,7 @@ async function getMatch_gameCode(gameCode) {
     }
 }
 
-//Search a match record base on gameCode
+//Add a match record base on gameCode
 async function addMatch(p1Minutes, p1Seconds, p1Milli, p1MatchScore,
     p2Minutes, p2Seconds, p2Milli, p2MatchScore, roundCount, p1, p2) {
     try {
@@ -85,11 +85,67 @@ async function addMatch(p1Minutes, p1Seconds, p1Milli, p1MatchScore,
     }
 }
 
+//Update match time on play or pause button hit
+async function updateMatch_time(p1Minutes, p1Seconds, p1Milli, 
+    p2Minutes, p2Seconds, p2Milli, gameCode) {
+    try {
+        let pool = await sql.connect(config);
+        let updateMatch_time = await pool.request()
+        .input('p1Minutes', sql.Int, p1Minutes)
+        .input('p1Seconds', sql.Int, p1Seconds)
+        .input('p1Milli', sql.Int, p1Milli)
+        .input('p2Minutes', sql.Int, p2Minutes)
+        .input('p2Seconds', sql.Int, p2Seconds)
+        .input('p2Milli', sql.Int, p2Milli)
+        .input('gameCode', sql.Int, gameCode)
+        .query("UPDATE Match SET p1Minutes = @p1Minutes, p1Seconds = @p1Seconds, p1Milli = @P1Milli, p2Minutes = @p2Minutes, p2Seconds = @p2Seconds, p2Milli = @p2Milli WHERE gameCode = @gameCode");
+        pool.close();
+        return updateMatch_time.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//Update match scores on submit round scores hit
+async function updateMatch_score(p1MatchScore, p2MatchScore, roundCount, gameCode) {
+    try {
+        let pool = await sql.connect(config);
+        let updateMatch_score = await pool.request()
+        .input('p1MatchScore', sql.Int, p1MatchScore)
+        .input('p2MatchScore', sql.Int, p2MatchScore)
+        .input('roundCount', sql.Int, roundCount) 
+        .input('gameCode', sql.Int, gameCode)
+        .query("UPDATE Match SET p1MatchScore = @p1MatchScore, p2MatchScore = @p2MatchScore, roundCount = @roundCount WHERE gameCode = @gameCode");
+        pool.close();
+        return updateMatch_score.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//Update match winner on Report match hit
+async function updateMatch_winner(winner, gameCode) {
+    try {
+        let pool = await sql.connect(config);
+        let updateMatch_winner = await pool.request()
+        .input('winner', sql.Int, winner) 
+        .input('gameCode', sql.Int, gameCode)
+        .query("UPDATE Match SET winner = @winner WHERE gameCode = @gameCode");
+        pool.close();
+        return updateMatch_winner.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
 
 module.exports ={
     getPlayers: getPlayers,
     getPlayer_tag: getPlayer_tag,
     addPlayer: addPlayer,
     getMatch_gameCode: getMatch_gameCode,
-    addMatch: addMatch
+    addMatch: addMatch,
+    updateMatch_time: updateMatch_time,
+    updateMatch_score: updateMatch_score,
+    updateMatch_winner: updateMatch_winner
 }
