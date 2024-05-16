068862468988d46a392759dc03ae4cd4e3455afc
cvSquiggles <jones.steven.alexander@gmail.com>
2024-05-15 20:47:58 -0400

    Built out connectivity and all api requests except the Match Update query.

--- api.js
+++ api.js
@@ -6,8 +6,81 @@ var Player = require('./player');
 //sql.connect(config);
 //
 
-console.log('Before the getPlayers call.');
-Db.getPlayers().then(result => {
-    console.log(result);
+// Express to create API, body-parser to parse request/response,
+// enable cross-origin resource sharing, and create express object app, and a router object
+var express = require('express');
+var bodyParser = require('body-parser')
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);  // Set base api route
+
+//Middleware always called before any other route
+//Authenitcation and input analysis can be handled here before submitting API request
+router.use((request, response, next) => {
+    console.log('middleware');
+    next();
 })
-console.log('After getPlayers call.');
+
+//Player Routes
+
+//Pull all players from database
+router.route('/players').get((request, response)=>{
+    Db.getPlayers().then(result => {
+        //console.log(result);
+        response.json(result[0]);
+    })
+})
+
+//Pull a specific player from database
+router.route('/player/:tag').get((request, response)=>{
+    Db.getPlayer_tag(request.params.tag).then(result => {
+        //console.log(result);
+        response.json(result[0]);
+    })
+})
+
+//Add a new player to the database
+router.route('/addPlayer/:tag').post((request, response)=>{
+    Db.addPlayer(request.params.tag).then(result => {
+        //console.log(result);
+        response.json(result).status(201);
+    })
+})
+
+//Match Routes
+
+//Pull a specific match from database
+router.route('/match/:gameCode').get((request, response)=>{
+    Db.getMatch_gameCode(request.params.gameCode).then(result => {
+        //console.log(result);
+        response.json(result);
+    })
+})
+
+//Add a new match to the database
+router.route('/addMatch/:p1Minutes/:p1Seconds/:p1Milli/:p1MatchScore/:p2Minutes/:p2Seconds/:p2Milli/:p2MatchScore/:roundCount/:p1/:p2').post((request, response)=>{
+    Db.addMatch(request.params.p1Minutes, request.params.p1Seconds, request.params.p1Milli,
+        request.params.p1MatchScore, request.params.p2Minutes, request.params.p2Seconds,
+        request.params.p2Milli, request.params.p2MatchScore, request.params.roundCount, request.params.p1, request.params.p2
+    ).then(result => {
+        //console.log(result);
+        response.json(result).status(201);
+    })
+})
+
+//Update an existing match in the database
+
+
+// Declare port to listen on for local testing
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log('Order API is running at ' + port);
+
+//Db.getPlayers().then(result => {
+//    console.log(result);
+//})
--- dboperations.js
+++ dboperations.js
@@ -2,18 +2,94 @@ var config = require('./dbconfig');
 const sql = require('mssql');
 
 async function getPlayers() {
-    console.log('starting run');
     try {
         let pool = await sql.connect(config);
-        console.log('We got the connect established.');
-        let players = pool.request().query("SELECT * FROM Player");
-        console.log('We have entered the query.');
-        return (await players).recordsets;
+        let players = await pool
+        .request()
+        .query("SELECT * FROM Player");
+        pool.close();
+        return players.recordsets;
     } catch(error){
         console.log(error);
     }
 }
 
+//Player API functions
+
+//Search a player record based on tag
+async function getPlayer_tag(tag) {
+    try {
+        let pool = await sql.connect(config);
+        let player = await pool.request()
+        .input('player_tag', sql.VarChar, tag)
+        .query("SELECT * FROM Player WHERE tag=@player_tag");
+        pool.close();
+        return player.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//Search a player record based on tag
+async function addPlayer(tag) {
+    try {
+        let pool = await sql.connect(config);
+        let addPlayer = pool.request()
+        .input('player_tag', sql.VarChar, tag)
+        .query('INSERT INTO Player VALUES (@player_tag)');
+        pool.close();
+        return addPlayer.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//Match API Functions
+
+//Search a match record base on gameCode
+async function getMatch_gameCode(gameCode) {
+    try {
+        let pool = await sql.connect(config);
+        let match = await pool.request()
+        .input('match_gameCode', sql.Int, gameCode)
+        .query("SELECT * FROM Match WHERE gameCode=@match_gameCode");
+        pool.close();
+        return match.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//Search a match record base on gameCode
+async function addMatch(p1Minutes, p1Seconds, p1Milli, p1MatchScore,
+    p2Minutes, p2Seconds, p2Milli, p2MatchScore, roundCount, p1, p2) {
+    try {
+        let pool = await sql.connect(config);
+        let addMatch = await pool.request()
+        .input('p1Minutes', sql.Int, p1Minutes)
+        .input('p1Seconds', sql.Int, p1Seconds)
+        .input('p1Milli', sql.Int, p1Milli)
+        .input('p1MatchScore', sql.Int, p1MatchScore)
+        .input('p2Minutes', sql.Int, p2Minutes)
+        .input('p2Seconds', sql.Int, p2Seconds)
+        .input('p2Milli', sql.Int, p2Milli)
+        .input('p2MatchScore', sql.Int, p2MatchScore)
+        .input('roundCount', sql.Int, roundCount)
+        .input('p1', sql.Int, p1)
+        .input('p2', sql.Int, p2)
+        .query("INSERT INTO Match VALUES (@p1Minutes, @p1Seconds, @p1Milli, @p1MatchScore, @p2Minutes, @p2Seconds, @p2Milli, @p2MatchScore, @roundCount, @p1, @p2, NULL)");
+        pool.close();
+        return addMatch.recordsets;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
 module.exports ={
-    getPlayers: getPlayers
+    getPlayers: getPlayers,
+    getPlayer_tag: getPlayer_tag,
+    addPlayer: addPlayer,
+    getMatch_gameCode: getMatch_gameCode,
+    addMatch: addMatch
 }
